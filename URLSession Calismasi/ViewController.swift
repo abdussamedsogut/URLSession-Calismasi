f1db7f3309ae2ca7db0b052d2af9dd5a34230ec2
Asamed <asamedsogut@hotmail.com>
2023-03-11 17:36:57 +0300

    DELETE ve UPDATE işlemleri gerçekleştirildi

--- URLSession Calismasi/ViewController.swift
+++ URLSession Calismasi/ViewController.swift
@@ -12,11 +12,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        kisiEkle()
-        
+        // kisiEkle()
+       // kisiSil()
+        kisiGuncelle()
     }
 
+    func kisiSil()  {
+        
+        var request = URLRequest(url: URL(string: "http://kasimadalan.pe.hu/kisiler/delete_kisiler.php")!)
+        
+        request.httpMethod = "POST"
+        
+        let postString = "kisi_id=119"
+        
+        request.httpBody = postString.data(using: .utf8)
+        // ust satirin ilk amaci postString olarak tanimlanan veriyi 'request'le tanimlanan web servise gondermek
+        // ikinci amaci gonderilen verinin turkce uyumlu hale gelmesini saglamak
 
+        
+        URLSession.shared.dataTask(with: request) { (data,response,error) in
+            
+            if error != nil || data == nil {
+                print("Hata")
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data!) as? [String:Any] {
+                    print(json)
+                }
+            } catch  {
+                print(error.localizedDescription)
+            }
+            
+        }.resume()
+      
+    }
     
     
     func kisiEkle()  {
@@ -48,6 +79,37 @@ class ViewController: UIViewController {
         
     }
     
+    func kisiGuncelle() {
+        
+        var request = URLRequest(url: URL(string: "http://kasimadalan.pe.hu/kisiler/update_kisiler.php")!)
+        
+        request.httpMethod = "POST"
+        
+        let postString = "kisi_id=14528&kisi_ad=TESTad&=&kisi_tel=TESTtelx"
+        
+        request.httpBody = postString.data(using: .utf8)
+        
+        URLSession.shared.dataTask(with: request) { (data,response,error) in
+            
+            if error != nil || data == nil {
+                print("Hata")
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data!) as? [String:Any] {
+                    print(json)
+                }
+            } catch  {
+                print(error.localizedDescription)
+            }
+            
+        }.resume()
+        
+        
+        
+        
+    }
     
     
     
